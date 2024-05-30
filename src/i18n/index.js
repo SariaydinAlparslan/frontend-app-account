e4bea7270b6f04205a0a2e9b0b37dd1454b913be
Alparslan Sarıaydın <alp@artistanbul.io>
2024-05-30 14:10:55 +0300

    added tr lang

---
+++ src/i18n/index.js
@@ -0,0 +1,34 @@
+import arMessages from './messages/ar.json';
+import deMessages from './messages/de.json';
+import es419Messages from './messages/es_419.json';
+import faIRMessages from './messages/fa_IR.json';
+import frMessages from './messages/fr.json';
+import frCAMessages from './messages/fr_CA.json';
+import hiMessages from './messages/hi.json';
+import itMessages from './messages/it.json';
+import ptMessages from './messages/pt.json';
+import ruMessages from './messages/ru.json';
+import ukMessages from './messages/uk.json';
+import zhcnMessages from './messages/zh_CN.json';
+import trMessages from './messages/tr_TR.json';
+import nlMessages from './messages/nl_NL.json';
+// no need to import en messages-- they are in the defaultMessage field
+
+const messages = {
+  ar: arMessages,
+  'es-419': es419Messages,
+  'fa-ir': faIRMessages,
+  fr: frMessages,
+  'zh-cn': zhcnMessages,
+  pt: ptMessages,
+  it: itMessages,
+  de: deMessages,
+  hi: hiMessages,
+  'fr-ca': frCAMessages,
+  ru: ruMessages,
+  uk: ukMessages,
+  'tr-tr': trMessages,
+  'nl-nl': nlMessages,
+};
+
+export default messages;
